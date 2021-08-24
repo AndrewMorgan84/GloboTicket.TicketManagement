5ac58e2f9bd60b6240524c3ddc13e4474a2199a9
Andrew Morgan <andrew.morgan@e.org>
2021-08-24 14:54:48 +0100

    Added Email contract and model

--- GloboTicket.TicketManagement.Application/Models/Mail/EmailSettings.cs
+++ GloboTicket.TicketManagement.Application/Models/Mail/EmailSettings.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GloboTicket.TicketManagement.Application.Models.Mail
+﻿namespace GloboTicket.TicketManagement.Application.Models.Mail
 {
     public class EmailSettings
     {
